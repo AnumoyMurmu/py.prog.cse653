c1131c0d5a9937d00caf00324591cbef9b9afcc6
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-02-28 17:03:15 +0530

    Update

---
+++ prog5.py
@@ -0,0 +1,13 @@
+#pyramid star pattern
+
+row=5
+
+for i in range(0, row):
+
+     for j in range(0, row-i-1):
+          print (end="")
+
+     for j in range(0, i+1):
+          print("",end="") 
+
+     print()
